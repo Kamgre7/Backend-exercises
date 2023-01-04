31e14b5bf0914a414fbdd008d6192e77f9bc1503
Kamil <kamgre7@gmail.com>
2023-01-04 19:16:22 +0100

    Solving task - findPhraseInArray

---
+++ be-fundamentals/findPhraseInArray-easy.ts
@@ -0,0 +1,17 @@
+type termArr = Array<[number, string]>;
+type findPhrase = termArr | string;
+
+export const findPhraseInArray = (
+  inputData: string[],
+  phrase: string
+): findPhrase => {
+  const phraseArr: termArr = [];
+
+  inputData.forEach((word, index) => {
+    if (word.includes(phrase)) {
+      phraseArr.push([index, word]);
+    }
+  });
+
+  return phraseArr.length > 0 ? phraseArr : 'Searched term does not exist';
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log('hello ');
+import { findPhraseInArray } from './be-fundamentals/findPhraseInArray-easy';
+
+const inputData = [
+  'create',
+  'volleyball',
+  'read',
+  'police',
+  'friday',
+  'update',
+  'lamp',
+  'use',
+  'delete',
+  'football',
+  'ampere',
+  'basketball',
+  'mouse',
+  'cat',
+  'monday',
+];
+
+console.log(findPhraseInArray(inputData, 'ball'));
