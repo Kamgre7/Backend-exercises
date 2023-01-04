c1c13b61039b2ae88c6877a558a3010a68485d7b
Kamil <kamgre7@gmail.com>
2023-01-04 19:54:16 +0100

    Updated solutions of findPhraseInArray

--- be-fundamentals/findPhraseInArray-easy.ts
+++ be-fundamentals/findPhraseInArray-easy.ts
@@ -8,9 +8,7 @@ export const findPhraseInArray = (
   const phraseArr: termArr = [];
 
   inputData.forEach((word, index) => {
-    if (word.includes(phrase)) {
-      phraseArr.push([index, word]);
-    }
+    if (word.includes(phrase)) phraseArr.push([index, word]);
   });
 
   return phraseArr.length > 0 ? phraseArr : 'Searched term does not exist';
