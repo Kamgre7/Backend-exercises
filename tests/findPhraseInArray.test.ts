cebb889d82dae16f094bb41c4f3c1db79645d05c
Kamil <kamgre7@gmail.com>
2023-01-11 18:59:01 +0100

    Mocked data used in findPhraseInArray

--- tests/findPhraseInArray.test.ts
+++ tests/findPhraseInArray.test.ts
@@ -1,34 +1,30 @@
-import {
-  findPhraseInArray,
-  inputData,
-} from '../be-fundamentals/functional-programming/findPhraseInArray-easy';
+import { findPhraseInArray } from '../be-fundamentals/functional-programming/findPhraseInArray-easy';
 
-test('function findPhraseInArray should be defined before use', () => {
-  expect(findPhraseInArray).toBeDefined();
-});
+// mockowanie danych
+// użyć describe
 
-test('array called to findPhraseInArray function should be defined', () => {
-  expect(inputData).toBeDefined();
-});
+const inputData = [
+  'create',
+  'volleyball',
+  'delete',
+  'football',
+  'ampere',
+  'basketball',
+  'mouse',
+];
 
-test('array is not null', () => {
-  expect(inputData).not.toBeNull();
-});
+describe('Test fn findPhraseInArray - final result when function find containing items, and when not found ', () => {
+  it('result contains items with "ball" term and index of word in array', () => {
+    expect(findPhraseInArray(inputData, 'ball')).toStrictEqual([
+      [1, 'volleyball'],
+      [3, 'football'],
+      [5, 'basketball'],
+    ]);
+  });
 
-test('First parameter should be an array', () => {
-  expect(inputData).toBeInstanceOf(Array);
-});
-
-test('result contains items with "ball" term and index of word in array', () => {
-  expect(findPhraseInArray(inputData, 'ball')).toStrictEqual([
-    [1, 'volleyball'],
-    [9, 'football'],
-    [11, 'basketball'],
-  ]);
-});
-
-test('Return string with information about not found items', () => {
-  expect(findPhraseInArray(inputData, 'test')).toBe(
-    'Searched term does not exist'
-  );
+  it('Return string with information about not found items', () => {
+    expect(findPhraseInArray(inputData, 'test')).toBe(
+      'Searched term does not exist'
+    );
+  });
 });
