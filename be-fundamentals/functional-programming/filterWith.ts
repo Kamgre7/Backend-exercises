345403f59de020ce36316e083afd4a9cff4928ef
Kamil <kamgre7@gmail.com>
2023-01-14 13:00:29 +0100

    Task - FilterWith

---
+++ be-fundamentals/functional-programming/filterWith.ts
@@ -0,0 +1,36 @@
+import { dataObj } from './data';
+
+const checkPhrase = (sentence: string, phrase: RegExp): boolean =>
+  phrase.test(sentence);
+
+function filterWith<T>(arr: T[], phrase: string): T[] {
+  const phraseRegExp = new RegExp(phrase, 'gi');
+
+  if (String(phrase).length <= 2) return [];
+
+  const filteredArr = arr.filter((element) => {
+    if (typeof element === 'object') {
+      let result = false;
+
+      for (const value of Object.values(element)) {
+        if (Array.isArray(value)) {
+          if (filterWith(value, phrase).length > 0) result = true;
+        } else {
+          if (checkPhrase(String(value), phraseRegExp)) result = true;
+        }
+      }
+
+      return result;
+    } else if (typeof element === 'string' || typeof element === 'number') {
+      return checkPhrase(String(element), phraseRegExp);
+    } else if (Array.isArray(element)) {
+      return filterWith(element, phrase).length > 0;
+    }
+  });
+
+  return filteredArr;
+}
+
+export const result = filterWith(dataObj, 'Delacruz Acevedo');
+export const result2 = filterWith(dataObj, 'Cummings Baxter');
+export const result3 = filterWith(dataObj, 'nisi');
