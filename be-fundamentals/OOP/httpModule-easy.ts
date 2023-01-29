170d0fa8621aaac4ffd7a5fc8c2beb98e11cd1a6
Kamil <kamgre7@gmail.com>
2023-01-29 15:43:24 +0100

    Added static methods get/post/delete to HttpService

--- be-fundamentals/OOP/httpModule-easy.ts
+++ be-fundamentals/OOP/httpModule-easy.ts
@@ -1,5 +1,60 @@
+import axios from 'axios';
+
+type userHeaders = {
+  [name: string]: string;
+};
+
 interface Cache {}
 
-class HttpService {}
+export class HttpService {
+  constructor() {}
+
+  static async get<T>(urlAddress: string, headers?: userHeaders): Promise<T> {
+    try {
+      const url = this.stringToUrl(urlAddress);
+
+      return headers
+        ? await axios.get(url.href, { headers })
+        : await axios.get(url.href);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async post<T>(
+    urlAddress: string,
+    body: unknown,
+    headers?: userHeaders
+  ): Promise<T> {
+    try {
+      const url = this.stringToUrl(urlAddress);
+
+      return headers
+        ? await axios.post(url.href, body, { headers })
+        : await axios.post(url.href, body);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async delete<T>(
+    urlAddress: string,
+    headers?: userHeaders
+  ): Promise<T> {
+    try {
+      const url = this.stringToUrl(urlAddress);
+
+      return headers
+        ? await axios.delete(url.href, { headers })
+        : await axios.delete(url.href);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  private static stringToUrl(link: string) {
+    return new URL(link);
+  }
+}
 
 class CacheService implements Cache {}
