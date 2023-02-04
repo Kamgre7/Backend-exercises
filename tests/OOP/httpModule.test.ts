3d30d45b3463b1c96edbffa6e7054e9c0615085f
Kamil <kamgre7@gmail.com>
2023-02-04 20:39:10 +0100

    Added unit test for task httpModule

---
+++ tests/OOP/httpModule.test.ts
@@ -0,0 +1,57 @@
+import { CacheService } from '../../be-fundamentals/OOP/httpModule/cacheService-class';
+import { HttpService } from '../../be-fundamentals/OOP/httpModule/httpService-class';
+
+const httpServiceGetMockSpy = jest
+  .spyOn(HttpService.prototype, 'get')
+  .mockImplementation(
+    async (url: string): Promise<any> =>
+      Promise.resolve({ status: 200, data: url })
+  );
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Testing httpModule task - using axios instance with design pattern Decorator', () => {
+  describe('httpService-class', () => {
+    it('Class should be defined', () => {
+      expect(HttpService).toBeDefined();
+    });
+
+    it('httpService get method should be called', async () => {
+      const httpClient = new HttpService();
+
+      await httpClient.get('http://localhost:80');
+
+      expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('CacheService class', () => {
+    it('Class should be defined', () => {
+      expect(CacheService).toBeDefined();
+    });
+
+    it('Method get of httpService should be called 1 times, next call from cacheUrlDb', async () => {
+      const httpClient = new HttpService();
+      const cacheUrlDb = new Map();
+      const newCacheService = new CacheService(httpClient, cacheUrlDb);
+
+      await newCacheService.get('http://example.com');
+      await newCacheService.get('http://example.com');
+
+      expect(newCacheService.httpService.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('Response should return { status: 200, data: urlLink }', async () => {
+      const cacheUrlDb = new Map();
+      const httpClient = new HttpService();
+      const newCacheService = new CacheService(httpClient, cacheUrlDb);
+
+      expect(await newCacheService.get('http://example.com')).toStrictEqual({
+        status: 200,
+        data: 'http://example.com',
+      });
+    });
+  });
+});
