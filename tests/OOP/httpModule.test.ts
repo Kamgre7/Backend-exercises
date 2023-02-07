d42056a029ad2caf23ee4e46ef7bb811d1a26a60
Kamil <kamgre7@gmail.com>
2023-02-07 20:41:54 +0100

    Mocked httpService with all methods

--- tests/OOP/httpModule.test.ts
+++ tests/OOP/httpModule.test.ts
@@ -1,12 +1,12 @@
 import { CacheService } from '../../be-fundamentals/OOP/httpModule/cacheService-class';
 import { HttpService } from '../../be-fundamentals/OOP/httpModule/httpService-class';
+import { HttpServiceInterface } from '../../be-fundamentals/OOP/httpModule/types';
 
-const httpServiceGetMockSpy = jest
-  .spyOn(HttpService.prototype, 'get')
-  .mockImplementation(
-    async (url: string): Promise<any> =>
-      Promise.resolve({ status: 200, data: url })
-  );
+const httpServiceMock: HttpServiceInterface<unknown> = {
+  delete: jest.fn().mockResolvedValue('linkTest'),
+  get: jest.fn().mockResolvedValue('linkTest'),
+  post: jest.fn().mockResolvedValue('linkTest'),
+};
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -19,37 +19,45 @@ describe('Testing httpModule task - using axios instance with design pattern Dec
     });
 
     it('httpService get method should be called', async () => {
-      const httpClient = new HttpService();
+      const httpClient = httpServiceMock;
 
-      await httpClient.get('http://localhost:80');
+      await httpClient.get('https://example.com', {});
 
       expect(httpClient.get).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('CacheService class', () => {
-    const httpClient = new HttpService();
     const cacheUrlDb = new Map();
-    const newCacheService = new CacheService(httpClient, cacheUrlDb);
+    const newCacheService = new CacheService(httpServiceMock, cacheUrlDb);
 
     it('Class should be defined', () => {
       expect(CacheService).toBeDefined();
     });
 
-    it('Method get of httpService should be called 1 times, next call from cacheUrlDb', async () => {
+    it('Method get of httpServiceMock should be called 1 times, next call from cacheUrlDb', async () => {
       await newCacheService.get('http://example.com');
       await newCacheService.get('http://example.com');
 
       expect(newCacheService.httpService.get).toHaveBeenCalledTimes(1);
+      expect(cacheUrlDb.size).toBe(1);
     });
 
-    it('Response should return { status: 200, data: urlLink }', async () => {
+    it('Method post of httpServiceMock should be called 2 times, and return linkTest', async () => {
+      const response = await newCacheService.post(
+        'http://example.com',
+        'randomData'
+      );
+      await newCacheService.post('http://example.com', 'randomData');
+
+      expect(newCacheService.httpService.post).toHaveBeenCalledTimes(2);
+      expect(response).toBe('linkTest');
+    });
+
+    it('Response should return linkTest', async () => {
       const response = await newCacheService.get('http://example.com');
 
-      expect(response).toStrictEqual({
-        status: 200,
-        data: 'http://example.com',
-      });
+      expect(response).toStrictEqual('linkTest');
     });
   });
 });
