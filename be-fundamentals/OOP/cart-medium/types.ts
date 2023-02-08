64586574a428861e4d5798230c83ae6ed8d71111
Kamil <kamgre7@gmail.com>
2023-02-08 20:25:40 +0100

    Product interface and Basket interface moved to class file

--- be-fundamentals/OOP/cart-medium/types.ts
+++ be-fundamentals/OOP/cart-medium/types.ts
@@ -1,34 +1,3 @@
-import { Product } from './product-class';
-
-export interface ProductInformation {
-  id: string;
-  name: string;
-  category: string;
-  price: number;
-  discount?: number;
-  finalPrice(): number;
-  changeName(newName: string): void;
-  changePrice(newPrice: number): void;
-  changeCategory(newCategory: string): void;
-}
-
-export type SingleProduct = {
-  name: string;
-  category: string;
-  price: number;
-};
-
-export interface BasketInformation {
-  id: string;
-  productList: Product[];
-  discount: number;
-
-  addProduct(item: Product): void;
-  removeProduct(id: string): void;
-  removeAllProduct(): void;
-  sumBasketPrice(): number;
-}
-
 export enum Discounts {
   NO_DISCOUNT = 0,
   TEN_PERCENT_DISCOUNT = 0.1,
