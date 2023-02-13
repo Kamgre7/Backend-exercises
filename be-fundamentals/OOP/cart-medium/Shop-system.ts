4304568faf3fe59e47988ab0ef564462b66a5879
Kamil <kamgre7@gmail.com>
2023-02-13 19:58:15 +0100

    Added methods to Shop-system class

--- be-fundamentals/OOP/cart-medium/Shop-system.ts
+++ be-fundamentals/OOP/cart-medium/Shop-system.ts
@@ -1,8 +1,23 @@
 import { v4 as uuid } from 'uuid';
+import { Basket } from './Basket';
+import { Product } from './Product';
+import { shopCategoryDb, shopProductDb } from './productExampleDB';
+import { currentDate } from './utils';
+
+interface SingleFinalizedOrder {
+  cart: Basket;
+  date: string;
+}
+
+type OrderHistory = [string, SingleFinalizedOrder][];
 
 export class ShopSystem {
-  private static instance: ShopSystem;
   readonly id: string = uuid();
+  private static instance: ShopSystem;
+  private static products: Product[] = shopProductDb;
+  private static finalizedOrderList = new Map<string, SingleFinalizedOrder>();
+  private static categories: string[] = shopCategoryDb;
+  private static usedDiscountCodes: [];
 
   private constructor() {}
 
@@ -13,4 +28,52 @@ export class ShopSystem {
 
     return ShopSystem.instance;
   }
+
+  static finalizeOrder(cart: Basket): void {
+    ShopSystem.restockProductQuantity(cart);
+
+    ShopSystem.finalizedOrderList.set(cart.id, { cart, date: currentDate() });
+  }
+
+  static showProducts(): Product[] {
+    return ShopSystem.products;
+  }
+
+  static addProduct(newProduct: Product): void {
+    this.products.push(newProduct);
+  }
+
+  static removeProduct(id: string): void {
+    this.products = this.products.filter((product) => product.id !== id);
+  }
+
+  static showCategories(): string[] {
+    return ShopSystem.categories;
+  }
+
+  static addCategory(newCategory: string): void {
+    this.categories.push(newCategory);
+  }
+
+  static removeCategory(name: string): void {
+    this.categories = this.categories.filter(
+      (category) => !category.includes(name)
+    );
+  }
+
+  static showFinalizedOrders(): OrderHistory {
+    return [...this.finalizedOrderList];
+  }
+
+  private static restockProductQuantity(cart: Basket): void {
+    cart.productList.forEach((cartItem) => {
+      const index = ShopSystem.products.findIndex(
+        (product) => cartItem.product.id === product.id
+      );
+
+      ShopSystem.products[index].restockQuantity(cartItem.amount);
+    });
+  }
 }
+
+// Discounts[Discounts.FIFTY_PERCENT_DISCOUNT];
