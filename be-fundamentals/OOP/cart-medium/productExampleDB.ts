21773e53743eb6e2ac99e61c3eeff9bbcc7a48b3
Kamil <kamgre7@gmail.com>
2023-02-13 19:56:08 +0100

    Added categories and export each product

--- be-fundamentals/OOP/cart-medium/productExampleDB.ts
+++ be-fundamentals/OOP/cart-medium/productExampleDB.ts
@@ -1,34 +1,34 @@
 import { Product } from './Product';
 
-const car = new Product({
+export const car = new Product({
   name: 'Fiat',
   category: 'Car',
   price: 1000,
   quantity: 10,
 });
 
-const smartphone = new Product({
+export const smartphone = new Product({
   name: 'Nokia',
   category: 'Smartphone',
   price: 50,
   quantity: 57,
 });
 
-const watch = new Product({
+export const watch = new Product({
   name: 'Garmin',
   category: 'Watch',
   price: 2000,
   quantity: 200,
 });
 
-const book = new Product({
+export const book = new Product({
   name: 'Harry Potter',
   category: 'Book',
   price: 17,
   quantity: 7,
 });
 
-const bread = new Product({
+export const bread = new Product({
   name: 'Bread',
   category: 'Food',
   price: 2,
@@ -36,3 +36,10 @@ const bread = new Product({
 });
 
 export const shopProductDb: Product[] = [car, smartphone, book, bread, watch];
+export const shopCategoryDb: string[] = [
+  'Watch',
+  'Car',
+  'Smartphone',
+  'Book',
+  'Food',
+];
