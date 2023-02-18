3775e3aabc1762e16620f21ca9fb44adcec88981
Kamil <kamgre7@gmail.com>
2023-02-18 13:17:06 +0100

    Refactored function currentDate - used toLocaleDateString

--- be-fundamentals/OOP/cart-medium/utils.ts
+++ be-fundamentals/OOP/cart-medium/utils.ts
@@ -1,2 +1,6 @@
 export const currentDate = (): string =>
-  new Date().toISOString().slice(0, 19).replace('T', ' ');
+  new Date().toLocaleDateString('pl-EU', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
