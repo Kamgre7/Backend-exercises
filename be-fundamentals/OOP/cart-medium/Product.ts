7c3b1177863d143f5eb51c9a00231eb6ab8e927f
Kamil <kamgre7@gmail.com>
2023-02-18 12:47:33 +0100

    Changed name of interface, removed quantity, methods: setQuantity and restockQuantity - added it to ShopSystem class

--- be-fundamentals/OOP/cart-medium/Product.ts
+++ be-fundamentals/OOP/cart-medium/Product.ts
@@ -1,29 +1,25 @@
 import { v4 as uuid } from 'uuid';
 import { Discounts } from './types';
 
-export interface ProductInformation {
+export interface IProduct {
   id: string;
   name: string;
   category: string;
   price: number;
-  quantity: number;
   discount: number;
   finalPrice(): number;
   setName(newName: string): void;
   setPrice(newPrice: number): void;
   setCategory(newCategory: string): void;
-  setQuantity(newQuantity: number): void;
-  restockQuantity(howManyLess: number): void;
 }
 
 export type SingleProduct = {
   name: string;
   category: string;
   price: number;
-  quantity: number;
 };
 
-export class Product implements ProductInformation {
+export class Product implements IProduct {
   readonly id: string;
   name: string;
   category: string;
@@ -31,14 +27,13 @@ export class Product implements ProductInformation {
   quantity: number;
   discount: Discounts = Discounts.NO_DISCOUNT;
 
-  constructor({ name, price, category, quantity }: SingleProduct) {
-    this.validateNewProduct({ name, price, category, quantity });
+  constructor({ name, price, category }: SingleProduct) {
+    this.validateNewProduct({ name, price, category });
 
     this.id = uuid();
     this.name = name;
     this.category = category;
     this.price = price;
-    this.quantity = quantity;
   }
 
   finalPrice(): number {
@@ -64,34 +59,24 @@ export class Product implements ProductInformation {
     this.category = newCategory;
   }
 
-  setQuantity(newQuantity: number): void {
-    this.checkIfNotBelowZero(newQuantity, 'quantity');
-    this.quantity = newQuantity;
-  }
-
-  restockQuantity(howManyLess: number) {
-    this.quantity -= howManyLess;
-  }
-
   private validateNewProduct(data: SingleProduct): void {
     this.checkIfNotBelowZero(data.price, 'price');
-    this.checkIfNotBelowZero(data.quantity, 'quantity');
 
     this.checkIfNotEmptyString(data.name, 'name');
     this.checkIfNotEmptyString(data.category, 'category');
   }
 
-  private checkIfNotBelowZero(amount: number, nameOfValue: string): void {
+  private checkIfNotBelowZero(amount: number, inputName: string): void {
     if (amount <= 0) {
-      throw new Error(`Cannot add item with ${nameOfValue} less than zero`);
+      throw new Error(`Cannot add item with ${inputName} less than zero`);
     }
   }
 
-  private checkIfNotEmptyString(name: string, nameOfValue: string): void {
+  private checkIfNotEmptyString(name: string, inputName: string): void {
     const nameTrim = name.trim();
 
     if (nameTrim.length === 0) {
-      throw new Error(`Cannot add item with empty ${nameOfValue}`);
+      throw new Error(`Cannot add item with empty ${inputName}`);
     }
   }
 }
