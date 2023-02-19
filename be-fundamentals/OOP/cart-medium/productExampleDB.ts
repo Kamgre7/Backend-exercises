aa9db48cb1fae7ebb120e424ede18b65881656f1
Kamil <kamgre7@gmail.com>
2023-02-19 20:42:46 +0100

    Removed quantity from type, added new type with product and quantity

--- be-fundamentals/OOP/cart-medium/productExampleDB.ts
+++ be-fundamentals/OOP/cart-medium/productExampleDB.ts
@@ -1,41 +1,44 @@
 import { Product } from './Product';
+import { ProductStock } from './Shop-system';
 
 export const car = new Product({
   name: 'Fiat',
   category: 'Car',
   price: 1000,
-  quantity: 10,
 });
 
 export const smartphone = new Product({
   name: 'Nokia',
   category: 'Smartphone',
   price: 50,
-  quantity: 57,
 });
 
 export const watch = new Product({
   name: 'Garmin',
   category: 'Watch',
   price: 2000,
-  quantity: 200,
 });
 
 export const book = new Product({
   name: 'Harry Potter',
   category: 'Book',
   price: 17,
-  quantity: 7,
 });
 
 export const bread = new Product({
   name: 'Bread',
   category: 'Food',
   price: 2,
-  quantity: 24,
 });
 
-export const shopProductDb: Product[] = [car, smartphone, book, bread, watch];
+export const shopProductDb: ProductStock[] = [
+  { product: car, quantity: 10 },
+  { product: smartphone, quantity: 57 },
+  { product: book, quantity: 200 },
+  { product: bread, quantity: 24 },
+  { product: watch, quantity: 7 },
+];
+
 export const shopCategoryDb: string[] = [
   'Watch',
   'Car',
