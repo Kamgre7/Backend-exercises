7a6d21fcef6c1502a0bf9dd073fef1fed5e9c06b
Kamil <kamgre7@gmail.com>
2023-02-26 16:03:15 +0100

    Added DataValidator class

--- be-fundamentals/OOP/cart-medium/utils.ts
+++ be-fundamentals/OOP/cart-medium/utils.ts
@@ -1,6 +1,30 @@
+import { Discounts } from './types';
+
 export const currentDate = (): string =>
   new Date().toLocaleDateString('pl-EU', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
   });
+
+export class DataValidation {
+  static checkIfNotEqualOrBelowZero(amount: number, inputName: string) {
+    if (amount <= 0) {
+      throw new Error(`Cannot add item with ${inputName} less or equal zero`);
+    }
+  }
+
+  static checkIfNotEmptyString(name: string, inputName: string): void {
+    const nameTrim = name.trim();
+
+    if (nameTrim.length === 0) {
+      throw new Error(`Cannot add item with empty ${inputName}`);
+    }
+  }
+
+  static checkIfDiscountLessThanValue(discount: Discounts) {
+    if (discount >= 1) {
+      throw new Error('Discount cannot be 100% or more');
+    }
+  }
+}
