81aa3e5ebb672a4ce3aa90b46f2284a79e04d2d6
Kamil <kamgre7@gmail.com>
2023-03-09 19:28:21 +0100

    Changed logDB and userDB type - was Array, now Map

--- be-fundamentals/OOP/loggerSystem/LoggerDB.ts
+++ be-fundamentals/OOP/loggerSystem/LoggerDB.ts
@@ -1,23 +1,29 @@
-import { Log, LogType } from './Log';
-import { User, UserRole } from './User';
+import { ILog, Log, LOG_TYPE } from './Log';
+import { IUser, User, USER_ROLE } from './User';
 
 const basicUser = new User('example@test.com');
-const adminUser = new User('admin@example.com');
-adminUser.role = UserRole.ADMIN;
+const adminUser = new User('admin@example.com', USER_ROLE.ADMIN);
 
 const warningLog = new Log({
-  type: LogType.WARNING,
+  type: LOG_TYPE.WARNING,
   content: 'Warning log!',
   createdBy: basicUser.id,
   permission: basicUser.role,
 });
 
 const errorLog = new Log({
-  type: LogType.ERROR,
+  type: LOG_TYPE.ERROR,
   content: 'Error log!',
   createdBy: adminUser.id,
   permission: adminUser.role,
 });
 
-export const LoggerDB: Log[] = [warningLog, errorLog];
-export const userDB: User[] = [basicUser, adminUser];
+export const logDB: Map<string, ILog> = new Map([
+  [warningLog.id, warningLog],
+  [errorLog.id, errorLog],
+]);
+
+export const userDB: Map<string, IUser> = new Map([
+  [basicUser.id, basicUser],
+  [adminUser.id, adminUser],
+]);
