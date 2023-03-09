41dfaeb34508fd6eeddfd571e97b08ac14ebac03
Kamil <kamgre7@gmail.com>
2023-03-09 18:08:52 +0100

    Changed enum UserRole, user role is transferred, now by 2nd argument when creating user

--- be-fundamentals/OOP/loggerSystem/User.ts
+++ be-fundamentals/OOP/loggerSystem/User.ts
@@ -1,24 +1,28 @@
 import { v4 as uuid } from 'uuid';
 import { emailRegex } from './utils';
 
-export enum UserRole {
+export enum USER_ROLE {
   OWNER = 'owner',
   ADMIN = 'admin',
   BASIC = 'basic',
 }
+
 interface IUser {
   id: string;
   email: string;
-  role: UserRole;
+  role: USER_ROLE;
   createdAt: Date;
 }
 
 export class User implements IUser {
   email: string;
-  role = UserRole.BASIC;
   createdAt = new Date();
 
-  constructor(email: string, public readonly id = uuid()) {
+  constructor(
+    email: string,
+    public readonly role = USER_ROLE.BASIC,
+    public readonly id = uuid()
+  ) {
     this.validateMail(email);
     this.email = email;
   }
