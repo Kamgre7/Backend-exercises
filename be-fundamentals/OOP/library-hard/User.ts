715c487275cea64001a5efef9c32c01c3d6eff73
Kamil <kamgre7@gmail.com>
2023-03-15 22:15:12 +0100

    Added User class

---
+++ be-fundamentals/OOP/library-hard/User.ts
@@ -0,0 +1,24 @@
+import { v4 as uuid } from 'uuid';
+import { emailRegex } from './utils';
+
+export interface IUser {
+  id: string;
+  email: string;
+  createdAt: Date;
+}
+
+export class User implements IUser {
+  email: string;
+  createdAt = new Date();
+
+  constructor(email: string, public readonly id = uuid()) {
+    this.validateMail(email);
+    this.email = email;
+  }
+
+  private validateMail(email: string): void {
+    if (!emailRegex.test(email)) {
+      throw new Error('Invalid email address');
+    }
+  }
+}
