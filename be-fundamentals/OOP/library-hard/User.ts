936cc34b1d112d977b6e1185221d488f729353a6
Kamil <kamgre7@gmail.com>
2023-03-18 17:24:31 +0100

    Added properties: updatedAt, deletedAt, blockedAt. Added method setEMail

--- be-fundamentals/OOP/library-hard/User.ts
+++ be-fundamentals/OOP/library-hard/User.ts
@@ -1,19 +1,33 @@
 import { v4 as uuid } from 'uuid';
 import { emailRegex } from './utils';
 
-export type IUser = {
+export interface IUser {
   id: string;
   email: string;
   createdAt: Date;
-};
+  updatedAt: Date;
+  deletedAt: Date;
+  blockedAt: Date;
+  setEmail(newEmail: string): void;
+}
 
 export class User implements IUser {
   email: string;
-  createdAt = new Date();
+  createdAt: Date = new Date();
+  updatedAt: Date;
+  deletedAt: Date;
+  blockedAt: Date;
 
   constructor(email: string, public readonly id = uuid()) {
     this.validateMail(email);
     this.email = email;
+  }
+
+  setEmail(newEmail: string): void {
+    this.validateMail(newEmail);
+
+    this.email = newEmail;
+    this.updatedAt = new Date();
   }
 
   private validateMail(email: string): void {
