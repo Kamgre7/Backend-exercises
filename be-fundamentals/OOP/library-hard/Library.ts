078bcd7cd049dfaf6176e5c1bbf11f2cfbd10a39
Kamil <kamgre7@gmail.com>
2023-03-21 17:57:56 +0100

    Updated Library Class

--- be-fundamentals/OOP/library-hard/Library.ts
+++ be-fundamentals/OOP/library-hard/Library.ts
@@ -1,112 +1,132 @@
-import { Book, BookDetails, IBook } from './Book';
-import { Booking, IBooking } from './Booking';
-import { bookDB, userDB } from './libraryDB';
-import { IUser, User } from './User';
-
-export type BookInformation = {
-  book: IBook;
-  isAvailable: boolean;
-};
-
-export type UserInformation = {
-  user: IUser;
-  borrowedBooks: Map<string, IBooking>;
-  points: number;
-};
+import { BookDetails } from './Book';
+import { BookingList, IBookingList } from './BookingList';
+import { BookList, IBookList } from './BookList';
+import { IUserList, UserList } from './UserList';
+import { BOOKING_TIME, DAY_IN_MILLISECONDS, MONTH_IN_DAYS } from './utils';
 
 export interface ILibrary {
-  books: Map<string, BookInformation>;
-  users: Map<string, UserInformation>;
-  addBook(bookDetails: BookDetails): string;
+  bookList: IBookList;
+  userList: IUserList;
+  bookingList: IBookingList;
+  addBook(bookDetails: BookDetails, quantity: number): string;
   deleteBook(bookId: string): void;
-  rentBook(bookId: string, userId: string): void;
-  returnBook(bookId: string, userId: string): void;
+  setBookQuantity(bookId: string, quantity: number): void;
   addUser(email: string): string;
-  findUser(userId: string): UserInformation;
-  findBook(bookId: string): BookInformation;
+  setUserEmail(userId: string, newEmail: string): void;
+  activateUser(userId: string): void;
+  rentBook(bookId: string, userId: string): string;
+  returnBook(bookingId: string, userId: string): void;
 }
 
 export class Library implements ILibrary {
-  books: Map<string, BookInformation>;
-  users: Map<string, UserInformation>;
+  private static instance: Library;
 
-  constructor(books = bookDB, users = userDB) {}
+  private constructor(
+    public bookList = BookList.getInstance(),
+    public userList = UserList.getInstance(),
+    public bookingList = BookingList.getInstance()
+  ) {}
 
-  addBook(bookDetails: BookDetails): string {
-    const newBook = new Book(bookDetails);
+  static getInstance(
+    bookList?: BookList,
+    userList?: UserList,
+    bookingList?: BookingList
+  ): Library {
+    if (!Library.instance) {
+      Library.instance = new Library(bookList, userList, bookingList);
+    }
 
-    return newBook.id;
+    return Library.instance;
+  }
+
+  addBook(bookDetails: BookDetails, quantity: number): string {
+    return this.bookList.addBook(bookDetails, quantity);
   }
 
   deleteBook(bookId: string): void {
-    const book = this.findBook(bookId);
-
-    this.checkBookToRemove(book);
-
-    this.books.delete(book.book.id);
+    return this.bookList.deleteBook(bookId);
   }
 
-  rentBook(bookId: string, userId: string): void {
-    const book = this.findBook(bookId);
-    const user = this.findUser(userId);
-
-    if (!this.isAvailable(book)) {
-      throw new Error('Book not available');
-    }
-
-    const reservation = new Booking(book.book.id);
-
-    user.borrowedBooks.set(book.book.id, reservation);
-    book.isAvailable = false;
-  }
-
-  returnBook(bookId: string, userId: string): void {
-    const book = this.findBook(bookId);
-    const user = this.findUser(userId);
-
-    if (!user.borrowedBooks.has(bookId)) {
-      throw new Error('Book not found');
-    }
-
-    book.isAvailable = true;
-    user.borrowedBooks.delete(bookId);
+  setBookQuantity(bookId: string, quantity: number): void {
+    return this.bookList.setBookQuantity(bookId, quantity);
   }
 
   addUser(email: string): string {
-    const newUser = new User(email);
+    return this.userList.addUser(email);
+  }
 
-    this.users.set(newUser.id, {
-      user: newUser,
-      borrowedBooks: new Map(),
-      points: 0,
+  setUserEmail(userId: string, newEmail: string): void {
+    return this.userList.setUserEmail(userId, newEmail);
+  }
+
+  rentBook(bookId: string, userId: string): string {
+    const book = this.bookList.findBook(bookId);
+    const user = this.userList.findUser(userId);
+
+    if (book.quantity < 1) {
+      throw new Error('Book not available');
+    }
+
+    if (!user.isActive || user.penaltyPoints >= 10) {
+      throw new Error('User cannot rent a book');
+    }
+
+    const bookingId = this.bookingList.addBooking({
+      bookId: book.book.id,
+      userId: user.user.id,
     });
 
-    return newUser.id;
+    this.bookList.decreaseBookQuantityByOne(book.book.id);
+
+    return bookingId;
   }
 
-  findUser(userId: string): UserInformation {
-    if (!this.users.has(userId)) {
-      throw new Error('User not found');
+  returnBook(bookingId: string, userId: string): void {
+    const booking = this.bookingList.findBooking(bookingId);
+    const user = this.userList.findUser(userId);
+    const book = this.bookList.findBook(booking.bookId);
+
+    if (!booking.isActive) {
+      throw new Error('Book already returned');
     }
 
-    return this.users.get(userId);
-  }
+    const currentDate = new Date();
+    const bookingDays = this.countDays(booking.getBookedDate(), currentDate);
 
-  findBook(bookId: string): BookInformation {
-    if (!this.books.has(bookId)) {
-      throw new Error('User not found');
+    if (bookingDays > BOOKING_TIME) {
+      user.penaltyPoints = bookingDays - BOOKING_TIME;
     }
 
-    return this.books.get(bookId);
-  }
-
-  private checkBookToRemove(book: BookInformation): void {
-    if (!this.isAvailable(book)) {
-      throw new Error('Cannot remove borrowed book!');
+    if (user.penaltyPoints >= 10) {
+      this.userList.blockUser(user.user.id);
     }
+
+    this.bookList.increaseBookQuantityByOne(book.book.id);
+    this.bookingList.setBookingReturned(booking.id);
   }
 
-  private isAvailable(book: BookInformation): boolean {
-    return book.isAvailable ? true : false;
+  activateUser(userId: string): void {
+    const user = this.userList.findUser(userId);
+    const currentDate = new Date();
+    const daysUserBlocked = this.countDays(user.user.blockedAt, currentDate);
+
+    if (!(daysUserBlocked > MONTH_IN_DAYS)) {
+      throw new Error(
+        `User cannot be activated. To activate ${
+          MONTH_IN_DAYS - daysUserBlocked
+        } left`
+      );
+    }
+
+    this.userList.activateUser(user.user.id);
+  }
+
+  private countDays(from: Date, to: Date): number {
+    const dayFrom = from.getTime();
+    const dayTo = to.getTime();
+
+    const differenceInDays = (dayTo - dayFrom) / DAY_IN_MILLISECONDS;
+
+    return Math.floor(differenceInDays);
   }
 }
