b3fcb4f733574a63bfc106680f4b60a13b3de6fe
Kamil <kamgre7@gmail.com>
2023-03-21 17:40:19 +0100

    Added bookingTime and monthInDays variable

--- be-fundamentals/OOP/library-hard/utils.ts
+++ be-fundamentals/OOP/library-hard/utils.ts
@@ -2,3 +2,5 @@ export const emailRegex =
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const DAY_IN_MILLISECONDS = 1000 * 3600 * 24;
+export const BOOKING_TIME = 7;
+export const MONTH_IN_DAYS = 30;
