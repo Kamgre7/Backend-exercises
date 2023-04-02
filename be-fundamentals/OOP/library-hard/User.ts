1e38bcf54a23ff290fd142c1440d343e5ceafa99
Kamil <kamgre7@gmail.com>
2023-04-02 21:32:37 +0200

    Updated User

--- be-fundamentals/OOP/library-hard/User.ts
+++ be-fundamentals/OOP/library-hard/User.ts
@@ -13,7 +13,7 @@ export interface IUser {
 
 export class User implements IUser {
   email: string;
-  readonly createdAt: Date = new Date();
+  createdAt: Date = new Date();
   updatedAt: Date;
   deletedAt: Date;
   blockedAt: Date;
