9751f382969e316ecad2b02536dcb020ca9fbed2
Kamil <kamgre7@gmail.com>
2023-04-04 22:23:54 +0200

    Updated userList

--- be-fundamentals/OOP/library-hard/UserList.ts
+++ be-fundamentals/OOP/library-hard/UserList.ts
@@ -4,19 +4,14 @@ import { IUser, User } from './User';
 export type UserInformation = {
   user: IUser;
   penaltyPoints: number;
-  isActive: boolean;
 };
 
 export interface IUserList {
   users: Map<string, UserInformation>;
   addUser(email: string): string;
-  createNewUser(email: string): string;
-  reactiveUser(userId: string): void;
   deleteUser(userId: string): void;
-  checkIfActive(userId: string): boolean;
   findUserOrThrow(userId: string): UserInformation;
-  checkIfEmailUsed(email: string): boolean;
-  checkIfUserDeleted(userId: string): boolean;
+  findUserIdByEmail(email: string): string;
 }
 
 export class UserList implements IUserList {
@@ -33,29 +28,12 @@ export class UserList implements IUserList {
   }
 
   addUser(email: string): string {
-    return this.checkIfEmailUsed(email)
-      ? this.reactiveUser(email)
-      : this.createNewUser(email);
-  }
-
-  createNewUser(email: string): string {
     const user = new User(email);
 
     this.users.set(user.id, {
       user,
       penaltyPoints: 0,
-      isActive: true,
     });
-
-    return user.id;
-  }
-
-  reactiveUser(email: string): string {
-    const userId = this.findUserIdByEmail(email);
-    const { user } = this.findUserOrThrow(userId);
-
-    user.deletedAt = null;
-    user.updatedAt = new Date();
 
     return user.id;
   }
@@ -63,11 +41,12 @@ export class UserList implements IUserList {
   deleteUser(userId: string): void {
     const { user } = this.findUserOrThrow(userId);
 
-    if (this.checkIfActive(userId)) {
-      user.deletedAt = new Date();
+    if (user.deletedAt) {
+      throw new Error('User already deleted');
     }
 
-    throw new Error('User already deleted');
+    user.deletedAt = new Date();
+    user.updatedAt = new Date();
   }
 
   findUserOrThrow(userId: string): UserInformation {
@@ -78,29 +57,11 @@ export class UserList implements IUserList {
     return this.users.get(userId);
   }
 
-  checkIfEmailUsed(email: string): boolean {
-    return [...this.users].some(
-      ([id, userInformation]) => userInformation.user.email === email
-    );
-  }
-
-  checkIfActive(userId: string): boolean {
-    const { user } = this.findUserOrThrow(userId);
-
-    return user.deletedAt === null;
-  }
-
-  checkIfUserDeleted(userId: string): boolean {
-    const { user } = this.findUserOrThrow(userId);
-
-    return user.deletedAt !== null;
-  }
-
-  private findUserIdByEmail(email: string): string {
-    const [userId] = [...this.users].find(
+  findUserIdByEmail(email: string): string {
+    const user = [...this.users].find(
       ([userId, userInformation]) => userInformation.user.email === email
     );
 
-    return userId;
+    return user ? user[0] : null;
   }
 }
