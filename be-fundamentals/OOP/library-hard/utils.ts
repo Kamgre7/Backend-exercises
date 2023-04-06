9111eae388066dc24739d90adfa459eb2acb10da
Kamil <kamgre7@gmail.com>
2023-04-06 20:05:14 +0200

    Added fn countDays, currentDate,

--- be-fundamentals/OOP/library-hard/utils.ts
+++ be-fundamentals/OOP/library-hard/utils.ts
@@ -4,5 +4,16 @@ export const emailRegex =
 export const dateFormatRegex = /^\d{4}[\/.]\d{1,2}[\/.]\d{1,2}$/;
 
 export const DAY_IN_MILLISECONDS = 1000 * 3600 * 24;
-export const BOOKING_TIME = 7;
+export const ALLOWED_BOOKING_TIME = 7;
 export const TIME_PERIOD_TO_UNBLOCK = 30;
+
+export const currentDate = () => new Date();
+
+export const countDays = (from: Date, to: Date): number => {
+  const dayFrom = from.getTime();
+  const dayTo = to.getTime();
+
+  const differenceInDays = (dayTo - dayFrom) / DAY_IN_MILLISECONDS;
+
+  return Math.floor(differenceInDays);
+};
