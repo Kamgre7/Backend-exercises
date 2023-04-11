d30aa0325d909bf9437d9bcd67f8d5bab59c777e
Kamil <kamgre7@gmail.com>
2023-04-11 22:45:12 +0200

    Added bookHandler uni tests

---
+++ tests/OOP/library-task/bookHandler.test.ts
@@ -0,0 +1,86 @@
+import { Book } from '../../../be-fundamentals/OOP/library-hard/Book';
+import { BookHandler } from '../../../be-fundamentals/OOP/library-hard/BookHandler';
+import { BookInformation } from '../../../be-fundamentals/OOP/library-hard/BookList';
+
+let bookHandler: BookHandler;
+let harryPotter: Book;
+let hpInformation: BookInformation;
+
+beforeAll(() => {
+  bookHandler = new BookHandler();
+});
+
+beforeEach(() => {
+  harryPotter = new Book({
+    title: 'Harry Potter',
+    author: 'J.K Rowling',
+    isbn: '1234',
+  });
+
+  hpInformation = {
+    book: harryPotter,
+    quantity: 10,
+  };
+});
+
+describe('BookHandler', () => {
+  it('Should be a instance of BookHandler', () => {
+    expect(bookHandler).toBeInstanceOf(BookHandler);
+  });
+
+  it('Should set book author', () => {
+    bookHandler.setAuthor(harryPotter, 'unknown');
+
+    expect(harryPotter.author).toBe('unknown');
+  });
+
+  it('Should set book title', () => {
+    bookHandler.setTitle(harryPotter, 'The Wither');
+
+    expect(harryPotter.title).toBe('The Wither');
+  });
+
+  it('Should set book isbn number', () => {
+    bookHandler.setIsbn(harryPotter, 'QWE123');
+
+    expect(harryPotter.isbn).toBe('QWE123');
+  });
+
+  it('Should update book quantity from 10 to 15', () => {
+    bookHandler.setQuantity(hpInformation, 5);
+
+    expect(hpInformation.quantity).toEqual(15);
+  });
+
+  describe('Should throw error when', () => {
+    it('Should throw error when updating book with empty author', () => {
+      expect(() => {
+        bookHandler.setAuthor(harryPotter, '');
+      }).toThrow();
+    });
+
+    it('Should throw error when updating book with empty title', () => {
+      expect(() => {
+        bookHandler.setTitle(harryPotter, '');
+      }).toThrow();
+    });
+
+    it('Should throw error when updating book with empty isbn number', () => {
+      expect(() => {
+        bookHandler.setIsbn(harryPotter, '');
+      }).toThrow();
+    });
+
+    it('Should throw error when updating book quantity and quantity is not integer number', () => {
+      expect(() => {
+        bookHandler.setQuantity(hpInformation, 7.42);
+      }).toThrow();
+    });
+
+    it('Should throw error when updating book quantity and quantity is below or equal zero', () => {
+      expect(() => {
+        bookHandler.setQuantity(hpInformation, -100);
+      }).toThrow();
+    });
+  });
+});
