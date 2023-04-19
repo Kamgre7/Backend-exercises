61917c4d0605edb95d36b4d704465544e4ff8feb
Kamil <kamgre7@gmail.com>
2023-04-19 21:21:21 +0200

    Added file with constants variables used in tests

---
+++ tests/OOP/library-task/utils/constants.ts
@@ -0,0 +1,29 @@
+import { BookDetails } from '../../../../be-fundamentals/OOP/library-hard/Book/Book';
+
+export const hpBookDetails: BookDetails = {
+  title: 'Harry Potter',
+  author: 'J.K Rowling',
+  isbn: '1234',
+};
+
+export const lotrBookDetails: BookDetails = {
+  title: 'Lord Of The Rings',
+  author: 'J.R.R Tolkien',
+  isbn: '4321',
+};
+
+export const witcherBookDetails: BookDetails = {
+  title: 'The Witcher',
+  author: 'A. Sapkowski',
+  isbn: 'BYS321',
+};
+
+export const bookData: BookDetails = {
+  author: 'Test',
+  isbn: 'ABC432',
+  title: 'Example',
+};
+
+export const kateUserEmail = 'kate@example.com';
+export const johnUserEmail = 'john@example.com';
+export const blockedUserEmail = 'blocked@example.com';
